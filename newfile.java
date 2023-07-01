2be22302fa385308f72c69bfc4e86a8f89ef6b11
Abhishekbotx <Abhishekbotx@gmail.com>
2023-07-01 07:53:28 +0530

    updated on1st july

---
+++ newfile.java
@@ -0,0 +1,13 @@
+class abhi {
+static main(){
+  system.out.println("hey peoples");
+// some codes
+
+<<<<<<< HEAD
+// newq linez on 16may
+
+=======
+//few mor lines for better understanding
+>>>>>>> 8198c29abfd62072d3a02d51ed762a5c44f2e3a1
+  }
+}
